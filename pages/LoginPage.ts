4c30c087e70bd5c0c2c93c39146ab82969376d02
Luke Simmers <luke.simmers@gmail.com>
2025-02-14 18:20:06 -0600

    Added dotenvx support

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -15,8 +15,8 @@ export class LoginPage {
       this.loginButton = page.locator('button[type="submit"]');
     }
   
-    async login(email: string, password: string) {
-      await this.usernameInput.fill(email);
+    async login(username: string, password: string) {
+      await this.usernameInput.fill(username);
       await this.passwordInput.fill(password);
       await this.loginButton.click();
     }
--- tests/setup.ts
+++
@@ -1,5 +0,0 @@
-// src/tests/setup.ts
-import dotenv from 'dotenv';
-
-// Load environment variables from .env file before any tests run
-dotenv.config();
